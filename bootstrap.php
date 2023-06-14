75ce0362616e53533a71a9e84b0d364f724eb0f9
Anderson M. de Carvalho <57923996+andersondevphp@users.noreply.github.com>
2023-06-14 15:16:09 -0300

    Add files via upload

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,12 @@ session_start();
 require "vendor/autoload.php";
 
 use Slim\App;
+use app\src\Whoops;
 
 $config['displayErrorDetails'] = true;
 
 $app = new App(['settings' => $config]);
+
+$whoops = new Whoops;
+
+$whoops->run($app);
